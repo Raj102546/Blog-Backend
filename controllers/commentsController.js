1db0117e6ae3ac707d2bf6072a80e8172574174b
Raj Vishwakarma <rajvish2001@gmail.com>
2026-07-07 16:29:54 +0530

    add jwt verify method

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,6 +1,14 @@
+const jwt = require("jsonwebtoken");
 const commentsDb = require("../database/commentsDb");
 
 exports.addCommentPost = async (req, res) => {
-  const comment = req.body;
-  await commentsDb.addComments({ comment });
+  jwt.verify(req.token, process.env.PRIVATE_KEY, (error, authData) => {
+    if (error) {
+      res.status(401).json({ error: error });
+    }
+    res.json({
+      message: "comment added",
+      authData,
+    });
+  });
 };
